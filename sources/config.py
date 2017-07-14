59174948c2d1c083ce05f3e7458839de3e585d2c
Elie Gavoty <eliegavoty@free.fr>
2017-07-14 03:26:43 +0200

    unprotect the path + correct static paths in app config

--- sources/config.py
+++ sources/config.py
@@ -7,8 +7,8 @@ class Config(object):
     SECRET_KEY = 't_ifJk2jhR,jl$0'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     QUESTION_ARCHIVE_DIR = 'question_archives'
-    MP3_DIR = './app/main/static/mp3'
-    ZIP_DIR = './app/main/static/zip'
+    MP3_DIR = './app/static/mp3'
+    ZIP_DIR = './app/static/zip'
     MESSAGES_ARCHIVE_FILENAME = 'messages.txt'
     
     CREDENTIALS = [
